1de68a7c8b2c78f0aadc3aa83cd92e6b1cfde404
Petra Spirkova <pspirkova@gmail.com>
2019-09-05 18:13:35 +0200

    add detail page and styles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 const BASE_URL = 'https://api.gavagai.se/v3';
 const API_KEY = '3acdef1f01cbceb88b132158abd466da';
+const DEFAULT_LANG = 'EN';
 
-function search() {
+function searchLexicon(mode) {
     const proxyurl = 'https://cors-anywhere.herokuapp.com/';
-    const word = document.getElementById('search').value;
-    const language = document.getElementById('language').value;
+    let word;
+    let language;
+    switch (mode) {
+        case 'similarWords':
+            word = document.getElementById('search').value;
+            if (word === '') {
+                return
+            }
+            language = document.getElementById('language').value;
+            if (language === ''){language = DEFAULT_LANG}
+            break;
+        case 'wordInfo':
+            const params = new URLSearchParams(document.location.search);
+            word = params.get('word');
+            language = params.get('language');
+    }
+
 fetch(`${proxyurl}${BASE_URL}/lexicon/${language}/${word}?apiKey=${API_KEY}&additionalFields=SEMANTICALLY_SIMILAR_WORDS`)
     .then(
         function (response) {
             response.json().then(function (data) {
-                const similarWords = document.getElementById('similar-words');
-                similarWords.innerHTML = renderList(data.semanticallySimilarWords);
+                switch (mode) {
+                    case 'similarWords':
+                        const similarWords = document.getElementById('similar-words');
+                        similarWords.innerHTML = renderList(data.semanticallySimilarWords);
+                        break;
+                    case 'wordInfo':
+                        const wordInfo = document.getElementById('word-info');
+                        if (data.wordInformation) {
+                            wordInfo.innerHTML = renderDetail(data.wordInformation);
+                        } else {
+                            wordInfo.innerHTML = `<span>This word has no info</span>`;
+                        }
+                }
+
             });
         }
     )
@@ -20,10 +48,26 @@ fetch(`${proxyurl}${BASE_URL}/lexicon/${language}/${word}?apiKey=${API_KEY}&addi
 }
 
 function renderList (semanticallySimilarWords) {
+    let language = document.getElementById('language').value;
+    if (language === ''){language = DEFAULT_LANG}
     return semanticallySimilarWords
         .map((item)=> {
-            return `<li>${item.word}</li>`
+            return `<a href="./detail.html?word=${item.word}&language=${language}">${item.word}</a>`
         })
         .join('')
 }
+
+function renderDetail (wordInfo) {
+return `<div>
+<h1>${wordInfo.word}</h1>
+<p><strong>Absolute Rank: </strong>${wordInfo.absoluteRank}</p>
+<strong>Related Info: </strong><a href="${wordInfo.additionalInformation.link}" target="_blank">${wordInfo.additionalInformation.link}</a>
+<p><strong>Document Frequency: </strong>${wordInfo.documentFrequency}</p>
+<p><strong>Frequency: </strong>${wordInfo.frequency}</p>
+<p><strong>Vocabulary Size: </strong>${wordInfo.vocabularySize}</p>
+
+</div>`
+}
+
+
 
